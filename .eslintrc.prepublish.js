8ae7375e9f4a013f79a5d58eb94f560cb92266dd
Raghav <dev@raghav98.com>
2023-02-22 23:05:51 -0500

    update version and publish

--- .eslintrc.prepublish.js
+++ .eslintrc.prepublish.js
@@ -1,13 +1,14 @@
 module.exports = {
-	extends: "./.eslintrc.js",
-	overrides: [
-		{
-			files: ['package.json'],
-			plugins: ['eslint-plugin-n8n-nodes-base'],
-			rules: {
-				'n8n-nodes-base/community-package-json-name-still-default': 'error',
-				'n8n-nodes-base/node-param-default-missing': 'off',
-			},
-		},
-	],
+    extends: "./.eslintrc.js",
+    overrides: [
+        {
+            files: ['package.json'],
+            plugins: ['eslint-plugin-n8n-nodes-base'],
+            rules: {
+                'n8n-nodes-base/community-package-json-name-still-default': 'error',
+                'n8n-nodes-base/node-param-default-missing': 'off',
+                'n8n-nodes-base/node-class-description-icon-not-svg': 'off',
+            },
+        },
+    ],
 };
